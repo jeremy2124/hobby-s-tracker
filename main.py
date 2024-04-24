6252b4c6c09cc074bac48a7791628a5008875e2c
jeremy2124 <133854718+jeremy2124@users.noreply.github.com>
2024-04-24 11:20:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from datetime import datetime
+
+USERNAME = ""
+TOKEN = ""
+GRAPH_ID = ""
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Coding Graph",
+    "unit": "hour",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+#day = datetime(year=2024, month=2, day=29)
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many hour did you code today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
